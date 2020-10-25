58e1b4bc67865b78ced83f3b58c69bc3cf969f8b
Ryan Bachman <43930050+5K-AN00BIS@users.noreply.github.com>
2020-10-25 11:37:13 -0700

    Add files via upload

---
+++ RyanB_AssignmentOne.py
@@ -0,0 +1,29 @@
+# Ryan Bachman
+# University of Advancing Technology
+# CSC235, Python 1, Fall 2020, Online 2
+# Your First Python Application Assignment (Week One)
+
+# Start Program
+# Begin print statements
+print() # Skips a line
+# Print out the header.
+print("'''---___---''' Ryan's Ultra Awesome First Python Program Tutorial '''---___---'''")
+print() # Skips a line
+# Print out instructions for printing in Python.
+print("In today's lesson, we will talk about how to print a line in Python.")
+print("In order to print a new line, type the following:")
+print("print('This is an example.')")
+print("Believe it or not, that's it!")
+# Print out separation line.
+print("--------------------------------------------------------------------------")
+# Print out instructions for commenting in Python
+print("Now let's talk about commenting your code.")
+print("In order to comment a section, just add a #. Simple as that!")
+print("For example:")
+print("# print('This is an example.')")
+print("The line above will not print anything to PowerShell, it's simply a way to comment your code!")
+# Print out separation line.
+print("--------------------------------------------------------------------------")
+print("This concludes my first tutorial and thus ends the assignment. Whew!")
+print() # Skips a line
+# End Program
